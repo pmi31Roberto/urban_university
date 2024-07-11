46b0b6caf781f95be6a450d488383dcc5fe6c444
Роберт <120023351+pmi31Roberto@users.noreply.github.com>
2024-07-11 20:30:42 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+total_tasks = 12
+hours_spent = 1.5
+name = 'Python'
+median_time = hours_spent / total_tasks
+print(name, ', всего задач:', total_tasks, ', затрачено часов:', hours_spent, ', среднее время выполнения:', median_time, '.')
