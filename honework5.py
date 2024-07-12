b16758768f010539fbc00ddd02d99cc47f26e07f
Роберт <120023351+pmi31Roberto@users.noreply.github.com>
2024-07-12 23:12:06 +0500

    Delete honework5.py

--- honework5.py
+++
@@ -1,9 +0,0 @@
-immutable_var = 1, True, 'star'
-print(immutable_var)
-# immutable_var[0] = qwerty
-# изменить кортеж нельзя но он может хранить изменяемые объекты.Например списки.
-mutable_list = [12, 'team', False]
-mutable_list[0] = 'tiger'
-mutable_list[1] = 123
-mutable_list[2] = True
-print(mutable_list)
