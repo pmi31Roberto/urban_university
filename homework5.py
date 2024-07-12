2c39f99271039f61bd28df7baac7d958036abefa
Роберт <120023351+pmi31Roberto@users.noreply.github.com>
2024-07-12 23:14:39 +0500

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,9 @@
+immutable_var = 1, True, 'star'
+print('Immutable tuple:', immutable_var)
+# immutable_var[0] = qwerty
+# изменить кортеж нельзя но он может хранить изменяемые объекты.Например списки.
+mutable_list = [12, 'team', False]
+mutable_list[0] = 'tiger'
+mutable_list[1] = 123
+mutable_list[2] = True
+print('Mutable list:', mutable_list)
